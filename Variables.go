4b3a78902e1530e535b1d4da42572f687e976e55
Holly Denstedt <63131767+HollyDenstedt@users.noreply.github.com>
2022-06-02 16:58:59 -0400

    Add files via upload

---
+++ Variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+  var i int   /* var = delcaring a variable, i = the name of the variable, int = the variable type  */
+  i = 42 /* assigning the variable */
+  var i int = 42 /*could also do this to make it less verbose */
+
+ /* OR*/
+  k := 66
+  fmt.Println(i)
+
+
+}
